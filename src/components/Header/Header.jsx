d8479a11218f3f801818e41eafd6745533325cc4
Dextrodark <faizalmohamed5302002@gmail.com>
2023-09-28 12:21:14 +0530

    Structure formed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+    import React from 'react';
+    import "./HEader.scss";
+
+    const Header = () => {
+    return (
+        <div>
+            Header
+        </div>
+    )
+    }
+
+    export default Header
